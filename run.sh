6bfa45363d3e68710097c4a8750561fbe44dec6d
Brotherhood <alessandro.berti94@hotmail.it>
2019-03-26 13:22:42 +0100

    init

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ then
         Image Name: thedeveloper.\n
         It could take few minutes"
     sudo docker build -t thedeveloper . 
-    echo "Create Persistent Storage in /home/developer"
+    echo "Creating Persistent Storage attached to /home/developer"
     sudo docker volume create --name persistent
     echo "Running.."
     sudo docker run --rm --net=host --ipc=host -e DISPLAY=$DISPLAY --volume="$HOME/.Xauthority:/root/.Xauthority:rw" -v peristent:/home/developer -it thedeveloper
